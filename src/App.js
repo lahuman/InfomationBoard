e033f82fa8fa3cc1caf53e4d80d9c035481ce25d
Daniel Lim <lahuman@daum.net>
2019-04-28 17:32:17 +0900

    update menu

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ class App extends React.Component {
       <Menu
         theme="dark"
         mode="horizontal"
-        defaultSelectedKeys={['2']}
+        defaultSelectedKeys={['0']}
         style={{ lineHeight: '64px' }}
       >
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
+        <Menu.Item key="1">저장</Menu.Item>
+        <Menu.Item key="2">불러오기</Menu.Item>
+        <Menu.Item key="3">안내판 쇼</Menu.Item>
       </Menu>
     </Header>
 
